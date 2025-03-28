62a798ec6259e8a4cbdf17d07f309e2797ee1c5c
Shadan20 <143886489+Shadan2002@users.noreply.github.com>
2025-03-28 03:54:09 +0000

    Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const bankId = 987659
+let accountEmail = "abcd@gmail.com"
+var accountPassword ="6575"
+accountCity="ranchi"
+let  accountState
+ 
+//bankId=6789 //not allowed
+accountEmail="advbhs@haa"
+accountPassword="11111"
+accountCity="delhi"
+/*
+prefer not to use var
+because of issue in block scope and function scope
+
+
+*/
+
+console.log(bankId)
+console.table([bankId,accountEmail,accountPassword,accountCity,accountState])
+
