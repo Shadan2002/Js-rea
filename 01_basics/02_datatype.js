e536b182b2644ac265c9ed5c00cba636092ec3ac
Shadan20 <143886489+Shadan2002@users.noreply.github.com>
2025-03-28 04:18:12 +0000

    Datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,19 @@
+"use strict" //treat all js code as newer version
+
+// alert(3+2) we are using nodejs not browser
+
+let name="shadan"
+let age= 18
+let isloggedIn=false
+
+//number =>2 to power 53
+//bigint
+//string =>""
+// boolean =>true/false
+//null =>standalone value
+//undefined =>
+//symbol =>unique
+//object
+console.log(typeof "shadan")
+console.log(typeof null)  //null is object 
+console.table([typeof "shadan", typeof 18, typeof false ]);
