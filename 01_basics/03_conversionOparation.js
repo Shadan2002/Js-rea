5459a6dcf44ad654260a2d8eb5d8e02f97ac63a6
Shadan20 <143886489+Shadan2002@users.noreply.github.com>
2025-03-28 04:41:32 +0000

    conversion of data type

---
+++ 01_basics/03_conversionOparation.js
@@ -0,0 +1,22 @@
+let score="33"
+console.log(typeof score)
+console.log(typeof(score))
+ let valueofNumber=Number(score)
+ console.log(typeof valueofNumber)
+
+
+ // "33"=> 33
+ //"33abc"=>NaN
+ // true=>1; false=>0
+ let isLoogedIn=1
+ let booleanIsloggedIn=Boolean(isLoogedIn)
+ console.log(booleanIsloggedIn)
+
+ // 1=>true; 0=>false
+ //""=>false
+ //"shadan"=>true
+ let somenumber= 33
+
+ let ConvertToString=String(somenumber)
+ console.log(ConvertToString)
+ console.log(typeof ConvertToString)
