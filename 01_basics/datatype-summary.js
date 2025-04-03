df4bb04db05639eb1f792607d6f8565ecf0484d4
Shadan20 <143886489+Shadan2002@users.noreply.github.com>
2025-04-03 14:55:16 +0000

    datatype-s

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,19 @@
+//primitive
+// 7 types:String,Number,Boolean,null,undefined,Symbol,BigInt
+
+
+const id=Symbol("123")
+const anotherId=Symbol("123")
+console.log(id===anotherId)
+const BigInt= 123456789009876543n
+//Referance(non primitive)
+//Array,Object,Function
+const heros=["salman",'srk','amir']//array
+let myObject={
+    name:"khan",
+    age:23,
+}
+
+const myFunction=function(){
+    console.log("hello world")
+}
