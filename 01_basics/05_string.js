ec12dee2cbb92922df6ab771f64174cf7ea5f7f5
Shadan20 <143886489+Shadan2002@users.noreply.github.com>
2025-04-08 06:51:25 +0000

    #

---
+++ 01_basics/05_string.js
@@ -0,0 +1,5 @@
+
+const name="shadan ahmad khan"
+const something=50
+//console.log(name+ something+ " value")
+console.log(`Hello my name is ${name} and my age is ${something}`)
--- 01_basics/Datatype-sum.js
+++ 01_basics/Datatype-sum.js
@@ -33,23 +33,3 @@ let userTwo=userOne
 userTwo.email="ahmad@gmai"
 console.log(userOne.email);
 console.log(userTwo.email); 
-// 
-//primitive
-// 7 types:String,Number,Boolean,null,undefined,Symbol,BigInt
-
-
-// const id=Symbol("123")
-// const anotherId=Symbol("123")
-// console.log(id===anotherId)
-// const BigInt= 123456789009876543n
-// //Referance(non primitive)
-// //Array,Object,Function
-// const heros=["salman",'srk','amir']//array
-// let myObject={
-//     name:"khan",
-//     age:23,
-// }
-
-// const myFunction=function(){
-//     console.log("hello world")
-// }
