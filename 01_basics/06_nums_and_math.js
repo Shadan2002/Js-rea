3177aa7d733e15aa05d0d7fe2ba5daa87e459f88
Shadan20 <143886489+Shadan2002@users.noreply.github.com>
2025-04-15 15:59:18 +0000

    Math and number

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,32 @@
+const score=400
+console.log(score);
+const balance =new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+const otherNumber=23.878;
+const otherNumber1=123.878;
+const otherNumber11=12113.878;
+//console.log(otherNumber.toPrecision(3))
+// console.log(otherNumber1.toPrecision(3))
+// console.log(otherNumber11.toPrecision(3))
+
+const hundreds=1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+//console.log(hundreds.toLocaleString());american
+
+
+//++++++++++++++++++++++++++++++++++++++Maths+++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-3));
+// console.log(Math.round(5.7));
+// console.log(Math.ceil(5.2));
+// console.log(Math.floor(5.2));
+// console.log(Math.min(8,6,0,5,67,-8))
+//console.log(Math.max(8,6,0,5,67,-8))
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+
+
+
