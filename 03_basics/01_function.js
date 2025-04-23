5b736542092bb852ac197d009b6398a88cdaeec7
Shadan20 <143886489+Shadan2002@users.noreply.github.com>
2025-04-23 14:33:20 +0000

    #

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -57,3 +57,4 @@ function returnarray(anyarr){
 
 }
 console.log(returnarray(newarr))
+
---
+++ 03_basics/02_scope.js
@@ -0,0 +1,15 @@
+//var c=300
+let a=300
+
+
+
+if(true){
+    let a=10
+    const b=90
+    console.log("Inner:",a)
+    //var c=89
+
+}
+console.log(a)
+//console.log(b)
+//console.log(c)
